d74068b29602178ff4d196c32aa1e614ad6daac5
sabarku <1363292650@qq.com>
2022-06-19 18:56:57 +0800

    1.使用maven+IDEA搭建springboot的开发环境 2.集成mybatis操作数据库，使用mybatis-generator自动生成脚本，解析实体类关系映射转化为mybatis的内部对象注入容器

---
+++ src/main/java/com/shao/miaoshaproject/App.java
@@ -0,0 +1,30 @@
+package com.shao.miaoshaproject;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ * 创建简单的web项目
+ */
+//自动化配置启动
+//@EnableAutoConfiguration 异曲同工之妙 ：将其进行托管并指定其为启动类
+@SpringBootApplication(scanBasePackages = {"com.shao.miaoshaproject"}) //将根目录下的包依次扫描，通过注解的方式发现Service，contriller等特定注解
+//做路径映射..
+@RestController
+@MapperScan("com.shao.miaoshaproject.dao") //dao存放的地方要放在这个注解上
+public class App 
+{
+    @RequestMapping("/")
+    public void test(){
+        System.out.println("ces");
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
